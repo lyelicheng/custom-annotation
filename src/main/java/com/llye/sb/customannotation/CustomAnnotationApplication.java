f83a3aeefe2471a8edc832eed8d62a98a98a14c7
joycelye <joycelye1991@gmail.com>
2023-11-25 17:07:11 +0800

    init

---
+++ src/main/java/com/llye/sb/customannotation/CustomAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.llye.sb.customannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llye/sb/customannotation/CustomAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llye.sb.customannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
